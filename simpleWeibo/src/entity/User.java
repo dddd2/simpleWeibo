d8579e379bd00e74d7d5224766840c5bc0109132
刘洋 <@DESKTOP-1MLN6GR>
2017-06-19 16:34:34 +0800

    dd

---
+++ simpleWeibo/src/entity/User.java
@@ -0,0 +1,64 @@
+package entity;
+
+public class User {
+	private int userId;
+	private String name;
+	private String sex;
+	private int age;
+	private String email;
+	private String phone;
+	private String remark;//个人简介
+	private String touxiang;
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public String getTouxiang() {
+		return touxiang;
+	}
+	public void setTouxiang(String touxiang) {
+		this.touxiang = touxiang;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+}
