61bcedbd20e12c05dc10727184eaae9ba2fb01de
刘洋 <@DESKTOP-1MLN6GR>
2017-06-19 15:23:49 +0800

    jar包

---
+++ simpleWeibo/src/entity/Comment.java
@@ -0,0 +1,29 @@
+package entity;
+/**
+ * 评论
+ * @author 刘洋
+ *
+ */
+public class Comment {
+	private int commentId;
+	private int messageId;
+	private int userId;
+	public int getCommentId() {
+		return commentId;
+	}
+	public void setCommentId(int commentId) {
+		this.commentId = commentId;
+	}
+	public int getMessageId() {
+		return messageId;
+	}
+	public void setMessageId(int messageId) {
+		this.messageId = messageId;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+}		
---
+++ simpleWeibo/src/entity/Message.java
@@ -0,0 +1,38 @@
+package entity;
+
+import java.util.Date;
+
+public class Message {
+	private int messageId;
+	private User user;
+	private String text;
+	private Date date;
+	public Message() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getMessageId() {
+		return messageId;
+	}
+	public void setMessageId(int messageId) {
+		this.messageId = messageId;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+}
